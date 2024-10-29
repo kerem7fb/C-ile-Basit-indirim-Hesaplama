e1f4e054e6a02c19166401ab95e7eb4e151b0bad
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2024-10-29 12:01:43 +0300

    Add files via upload

---
+++ İndirimliÜrün/İndirimliÜrün/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace İndirimliÜrün
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(72, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Etiket Fiyatı :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(270, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SandyBrown;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(112, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 74);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Yüzde 10 İndirim";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Salmon;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(244, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 74);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Yüzde 25 İndirim";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Firebrick;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(376, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 74);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Yüzde 50 İndirim";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(507, 170);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 74);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Yüzde 75 İndirim";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(72, 325);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "İndirim Miktarı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(252, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(252, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(72, 377);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Son Fiyat :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 539);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kampanyalı Ürün";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ İndirimliÜrün/İndirimliÜrün/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İndirimliÜrün
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double etiketfiyat, indirim, sonfiyat;
+            etiketfiyat = Convert.ToDouble(textBox1.Text);
+            indirim=etiketfiyat*0.10;
+            sonfiyat = etiketfiyat - indirim;
+            label3.Text = indirim.ToString();
+            label4.Text = sonfiyat.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double etiketfiyat, indirim, sonfiyat;
+            etiketfiyat = Convert.ToDouble(textBox1.Text);
+            indirim = etiketfiyat * 0.25;
+            sonfiyat = etiketfiyat - indirim;
+            label3.Text = indirim.ToString();
+            label4.Text = sonfiyat.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double etiketfiyat, indirim, sonfiyat;
+            etiketfiyat = Convert.ToDouble(textBox1.Text);
+            indirim = etiketfiyat * 0.50;
+            sonfiyat = etiketfiyat - indirim;
+            label3.Text = indirim.ToString();
+            label4.Text = sonfiyat.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double etiketfiyat, indirim, sonfiyat;
+            etiketfiyat = Convert.ToDouble(textBox1.Text);
+            indirim = etiketfiyat * 0.75;
+            sonfiyat = etiketfiyat - indirim;
+            label3.Text = indirim.ToString();
+            label4.Text = sonfiyat.ToString();
+        }
+    }
+}
